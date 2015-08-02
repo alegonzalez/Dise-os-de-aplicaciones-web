b7b37d09a32e03f998e66d0c20f7877ae733261b
alegonzalez <aoalvaradog@est.utn.ac.cr>
2015-08-02 02:57:02 -0600

    Creacion de invoice y es guardada en localstorage

---
+++ Proyecto2/create_invoice.js
@@ -0,0 +1,60 @@
+var INVOICE = INVOICE || 
+{
+	/*Donde  se va hacerse el array*/
+	client: function (datoInvoice) {
+		this.datoInvoice = datoInvoice ;
+
+		/*Guarda los datos en el localstorage*/
+		this.saveDate = function () {
+			/*Como se va a llamar el key de las facturas*/
+			localStorage.setItem("Invoice",JSON.stringify(datoInvoice));
+
+		};
+	},
+
+    /*Save es el boton de yes donde guarda la factura*/
+	save:function()
+	{
+
+		datoInvoice = new Array();
+
+		/*La cantidad de key que hay en el local storage*/
+		for (x = 0; x < localStorage.length-1; x++) {
+
+			/*Lo obtiene en un string*/
+			datoInvoice=localStorage.getItem(datoInvoice);
+			/*Lo convierte en Objeto*/
+			datoInvoice=JSON.parse(localStorage.getItem("Invoice"));
+		}
+
+		/*Obtiene la informacion de los input*/
+		var client = document.getElementById('createselectclient').value;
+		var jobDescription = document.getElementById('createjobdescriptioninvoice').value;
+		var date = document.getElementById('createdateinvoice').value;
+		var amont = document.getElementById('createamontinvoice').value;
+
+		
+		
+
+		/*Arreglo  de los objetos*/
+		var information = {'Client':client , 'Job Description': jobDescription, 'Date': date, 'Amont':amont};
+		
+		/*Se hizo un if por que el arreglo esta null*/
+		if(datoInvoice==null)
+		{
+		    datoInvoice=[];
+		}
+			/*Push lo que hace es mandar los datos a lo ultimo*/
+			datoInvoice.push(information);
+
+	
+			/*Instancia donde se le envia los datos de las facturas*/
+			var datos = new INVOICE.client(datoInvoice);
+
+			/*donde se van a guardar los datos ya en el loca storage en su propio key*/
+			datos.saveDate();
+	},
+
+
+
+};
