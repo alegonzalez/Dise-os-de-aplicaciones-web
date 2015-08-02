7919608c7854caeb9e241c8c76d645cac4f816b8
alegonzalez <aoalvaradog@est.utn.ac.cr>
2015-08-02 03:16:04 -0600

    Creación de chambas
    Permite ingresar los datos y guardarlos en el localstorage.

---
+++ Proyecto2/create_chambas.js
@@ -0,0 +1,57 @@
+var CREATE_CHAMBAS = CREATE_CHAMBAS || 
+{
+	/*Donde  se va hacerse el array*/
+	client: function (datechambas) {
+		this.datechambas = datechambas ;
+
+		/*Guarda los datos en el localstorage*/
+		this.saveDate = function () {
+			/*Como se va a llamar el key de las facturas*/
+			localStorage.setItem("chambas",JSON.stringify(datechambas));
+
+		};
+	},
+
+    /*Save es el boton de yes donde guarda la creacion de chambas*/
+	save:function()
+	{
+
+		datechambas = new Array();
+
+		/*La cantidad de key que hay en el local storage*/
+		for (x = 0; x < localStorage.length-1; x++) {
+
+			/*Lo obtiene en un string*/
+			datechambas=localStorage.getItem(datechambas);
+			/*Lo convierte en Objeto*/
+			datechambas=JSON.parse(localStorage.getItem("chambas"));
+		}
+
+		/*Obtiene la informacion de los input*/
+		var client = document.getElementById('createselectclientchamba').value;
+		var jobDescription = document.getElementById('createjobdescriptionchamba').value;
+		var date = document.getElementById('createdatechamba').value;
+		var note = document.getElementById('createnotechamba').value;
+         
+         /*Arreglo  de los objetos*/
+		var information = {'Client':client , 'Job Description': jobDescription, 'Date': date, 'Note':note};
+		
+		/*Se hizo un if por que el arreglo esta null*/
+		if(datechambas==null)
+		{
+		    datechambas=[];
+		}
+			/*Push lo que hace es mandar los datos a lo ultimo*/
+			datechambas.push(information);
+
+	
+			/*Instancia donde se le envia los datos de las facturas*/
+			var datos = new CREATE_CHAMBAS.client(datechambas);
+
+			/*donde se van a guardar los datos ya en el loca storage en su propio key*/
+			datos.saveDate();
+	},
+
+
+
+};
