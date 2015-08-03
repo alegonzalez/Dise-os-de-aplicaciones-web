de5406117ed99a1207bb46783edbdc1ffa6d0a3a
alegonzalez <aoalvaradog@est.utn.ac.cr>
2015-08-02 18:38:33 -0600

    Se le agrega el guion
    Se agrego el guion ya que un espacio entre variable se cae el programa

--- Proyecto2/create_client.js
+++ Proyecto2/create_client.js
@@ -37,7 +37,7 @@ var CLIENT = CLIENT ||
 		console.log(identification);
 
 		/*Arreglo  de los objetos*/
-		var information = {'Identification':identification , 'Firts name': firstName, 'Last name': lastName, 'Phone':phone};
+		var information = {'Identification':identification , 'Firts_name': firstName, 'Last_name': lastName, 'Phone':phone};
 		
 		/*Se hizo un if por que el arreglo esta null*/
 		if(datoClient==null)
@@ -47,9 +47,6 @@ var CLIENT = CLIENT ||
 			/*Push lo que hace es mandar los datos a lo ultimo*/
 			datoClient.push(information);
 
-				console.log(datoClient);
-
-	
 			/*Instancia donde se le envia los datos de los clientes*/
 			var datos = new CLIENT.client(datoClient);
 
