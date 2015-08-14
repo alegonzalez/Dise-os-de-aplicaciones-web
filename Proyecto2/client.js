f0864d39d91583d965e0256a80dc53096f578cbc
alegonzalez <aoalvaradog@est.utn.ac.cr>
2015-08-14 15:41:33 -0600

    Cambio de estructura

--- Proyecto2/client.js
+++ Proyecto2/client.js
@@ -1,43 +1,50 @@
 var CLIENT=CLIENT||{
-/*Esta funcion sirve para saber los datos que se van a obtener segun 
-el la fila seleccionada
-*/
+
  capturarPosicion: function () {
 
   var table = document.getElementById("table_client");
-
   var rows = table.getElementsByTagName("tr");
-
+  
   for (i = 1; i < rows.length; i++) {
     row = table.rows[i];
     row.onclick = function(){
-
       var cell0 = this.getElementsByTagName("td")[0];
       var cell1= this.getElementsByTagName("td")[1];
       var cell2= this.getElementsByTagName("td")[2];
       var cell3= this.getElementsByTagName("td")[3];
-
+      
       var id=cell0.innerHTML;
       var firts_name = cell1.innerHTML;
       var last_Name=cell2.innerHTML;
       var phone=cell3.innerHTML;
 
-      
-      var dato=new Array();
-      var elementos_edit={'Identification':id,'Firts_name':firts_name,'Last_name':last_Name,"Phone":phone};
-      dato.push(elementos_edit);
+      var obtener_Datos=new CLIENT.localStorageTemporal(id,firts_name,last_Name,phone);
 
       
-      localStorage.setItem("temporal",JSON.stringify(dato));
-
       
-      var send=new DELETE_CLIENT.obtenerDatos(firts_name,last_Name);
-      
-    };
+    }
   }
 
 
-}
+},
+
+localStorageTemporal :function(id,firts_name,last_Name,phone)
+{
+
+  var datosTemporales=new Array();
+
+  this.id=id;
+  this.firts_name=firts_name;
+  this.last_Name=last_Name;
+  this.phone=phone;
+
+  var date={'Identification':this.id,'Firts_name':this.firts_name,'Last_name':this.last_Name,"Phone":this.phone};
+  
+  datosTemporales.push(date);
+
+  localStorage.setItem("Temporal",JSON.stringify(datosTemporales));
+
+},
 
 };
 
@@ -70,8 +77,23 @@ el la fila seleccionada
      firts_name.innerHTML =datoclient[i].Firts_name;
      fullName.innerHTML =datoclient[i].Last_name;
      phone.innerHTML=datoclient[i].Phone;
-     action.innerHTML ='<a href="delete_client.html" onClick="CLIENT.capturarPosicion(); DELETE_CLIENT.obtenerDatos()"><img src="https://cdn3.iconfinder.com/data/icons/streamline-icon-set-free-pack/48/Streamline-70-32.png"style="margin-left:10%;"></img></a><a href="edit_client.html" onClick="CLIENT.capturarPosicion();"><img src="https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/32/edit_user.png" style="margin-left:10%;"></img></a>';
+     action.innerHTML ='<a href="delete_client.html" onClick="CLIENT.capturarPosicion(); "><img src="https://cdn3.iconfinder.com/data/icons/streamline-icon-set-free-pack/48/Streamline-70-32.png"style="margin-left:10%;"></img></a><a href="edit_client.html" onClick="CLIENT.capturarPosicion();"><img src="https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/32/edit_user.png" style="margin-left:10%;"></img></a>';
    }
-
+/*Oculta la opcion de user si no es el administrador*/
+  adm=localStorage.getItem("Login");
+  adm = JSON.parse(localStorage.getItem("Login"));
+  if(adm[0].Sesion!=1)
+  {
+    $(".menu_user").hide();
+  }
  })();
+
+
+
+
+ 
+ 
+ 
+
+ 
 
